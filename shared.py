cf69a48af2c0e8d95f81b5dd389266737393cb47
TechTechcy <techtech.cy@gmail.com>
2025-11-01 12:04:09 +0200

    Oops (forgot to make the ui only initalize if the python installation can support a ui)

--- shared.py
+++ shared.py
@@ -5,80 +5,87 @@ import inspect
 import logging
 import datetime
 
-##################################################### Logging Window ('borrowed' from stackoverflow) #####################################################
-import tkinter as tk
-import tkinter.scrolledtext as ScrolledText
-
-class TextHandler(logging.Handler):
-    # This class allows you to log to a Tkinter Text or ScrolledText widget
-    # Adapted from Moshe Kaplan: https://gist.github.com/moshekaplan/c425f861de7bbf28ef06
-
-    def __init__(self, text):
-        # run the regular Handler __init__
-        logging.Handler.__init__(self)
-        # Store a reference to the Text it will log to
-        self.text = text
-
-    def emit(self, record):
-        msg = self.format(record)
-        def append():
-            self.text.configure(state='normal')
-            self.text.insert(tk.END, msg + '\n')
-            self.text.configure(state='disabled')
-            # Autoscroll to the bottom
-            self.text.yview(tk.END)
-        # This is necessary because we can't modify the Text from other threads
-        self.text.after(0, append)
-        
-        
 class cfg:
     should_crash = True
+    
+    
+##################################################### Logging Window ('borrowed' from stackoverflow) #####################################################
+try:
+    import tkinter as tk
+    import tkinter.scrolledtext as ScrolledText
 
-class myGUI(tk.Frame):
+    class TextHandler(logging.Handler):
+        # This class allows you to log to a Tkinter Text or ScrolledText widget
+        # Adapted from Moshe Kaplan: https://gist.github.com/moshekaplan/c425f861de7bbf28ef06
 
-    # This class defines the graphical user interface 
+        def __init__(self, text):
+            # run the regular Handler __init__
+            logging.Handler.__init__(self)
+            # Store a reference to the Text it will log to
+            self.text = text
 
-    def __init__(self, parent, *args, **kwargs):
-        tk.Frame.__init__(self, parent, *args, **kwargs)
-        self.root = parent
-        self.build_gui()
-
-    def build_gui(self):                    
-        # Build GUI
-        self.root.title('TEST')
-        self.root.option_add('*tearOff', 'FALSE')
-        self.grid(column=0, row=0, sticky='ew')
-        self.grid_columnconfigure(0, weight=1, uniform='a')
-        self.grid_columnconfigure(1, weight=1, uniform='a')
-        self.grid_columnconfigure(2, weight=1, uniform='a')
-        self.grid_columnconfigure(3, weight=1, uniform='a')
-
-        # Add text widget to display logging info
-        st = ScrolledText.ScrolledText(self, state='disabled')
-        st.configure(font='TkFixedFont')
-        st.grid(column=0, row=1, sticky='w', columnspan=4)
-
-        # Create textLogger
-        text_handler = TextHandler(st)
-
-        # Logging configuration
-        logging.basicConfig(filename='test.log',
-            level=logging.INFO, 
-            format='%(asctime)s - %(levelname)s - %(message)s')        
-
-        # Add the handler to logger
-        logger = logging.getLogger()        
-        logger.addHandler(text_handler)
-
-    @staticmethod
-    def worker(logger: queue.Queue):
-        # Skeleton worker function, runs in separate thread
-        while True:
-            time = f"{datetime.datetime.now().hour}:{datetime.datetime.now().minute}:{datetime.datetime.now().second}"
-            raw_msg = logger.get()
-            msg = f"{time}: {raw_msg}"
+        def emit(self, record):
+            msg = self.format(record)
+            def append():
+                self.text.configure(state='normal')
+                self.text.insert(tk.END, msg + '\n')
+                self.text.configure(state='disabled')
+                # Autoscroll to the bottom
+                self.text.yview(tk.END)
+            # This is necessary because we can't modify the Text from other threads
+            self.text.after(0, append)
             
-            logging.info(msg)
+    class myGUI(tk.Frame):
+
+        # This class defines the graphical user interface 
+
+        def __init__(self, parent, *args, **kwargs):
+            tk.Frame.__init__(self, parent, *args, **kwargs)
+            self.root = parent
+            self.build_gui()
+
+        def build_gui(self):                    
+            # Build GUI
+            self.root.title('TEST')
+            self.root.option_add('*tearOff', 'FALSE')
+            self.grid(column=0, row=0, sticky='ew')
+            self.grid_columnconfigure(0, weight=1, uniform='a')
+            self.grid_columnconfigure(1, weight=1, uniform='a')
+            self.grid_columnconfigure(2, weight=1, uniform='a')
+            self.grid_columnconfigure(3, weight=1, uniform='a')
+
+            # Add text widget to display logging info
+            st = ScrolledText.ScrolledText(self, state='disabled')
+            st.configure(font='TkFixedFont')
+            st.grid(column=0, row=1, sticky='w', columnspan=4)
+
+            # Create textLogger
+            text_handler = TextHandler(st)
+
+            # Logging configuration
+            logging.basicConfig(filename='test.log',
+                level=logging.INFO, 
+                format='%(asctime)s - %(levelname)s - %(message)s')        
+
+            # Add the handler to logger
+            logger = logging.getLogger()        
+            logger.addHandler(text_handler)
+
+        @staticmethod
+        def worker(logger: queue.Queue):
+            # Skeleton worker function, runs in separate thread
+            while True:
+                time = f"{datetime.datetime.now().hour}:{datetime.datetime.now().minute}:{datetime.datetime.now().second}"
+                raw_msg = logger.get()
+                msg = f"{time}: {raw_msg}"
+                
+                logging.info(msg)
+except:
+    print("Failed to initalize ui, continuing without ui...")
+        
+        
+
+
             
             
 #####################################################################################################################################################
